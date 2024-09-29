706b8a85044d78a9e6bd4bbbebc5a9e017bc3df2
Tripop Netpu <150440919+TripopN@users.noreply.github.com>
2024-09-29 15:36:29 +0000

    3rd commit.

--- madt-final-project/dags/etl.py
+++ madt-final-project/dags/etl.py
@@ -1,234 +1,94 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from airflow.operators.empty import EmptyOperator
-from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
 from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
-from airflow.providers.google.cloud.transfers.gcs_to_gcs import GCSToGCSOperator
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+from airflow.providers.google.cloud.hooks.gcs import GCSHook
 from airflow.operators.dummy import DummyOperator
 from airflow.utils import timezone
-from airflow.contrib.operators.bigquery_operator import BigQueryOperator
-from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
-from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.utils.task_group import TaskGroup
+import datetime
+import os
+
+# Function to download, clean, and re-upload CSV files from raw bucket to storage bucket
+def clean_and_transfer_csv(source_bucket: str, source_object: str, destination_bucket: str, gcp_conn_id: str):
+    gcs_hook = GCSHook(gcp_conn_id=gcp_conn_id)
+
+    # Download the CSV file from source GCS bucket
+    local_file_path = f"/tmp/{source_object}"
+    gcs_hook.download(source_bucket, source_object, local_file_path)
+
+    # Clean the CSV file by removing null characters
+    cleaned_file_path = f"/tmp/cleaned_{source_object}"
+    with open(local_file_path, 'r', encoding='utf-8') as infile:
+        with open(cleaned_file_path, 'w', encoding='utf-8') as outfile:
+            for line in infile:
+                cleaned_line = line.replace('\x00', '')  # Remove null characters
+                outfile.write(cleaned_line)
+
+    # Upload the cleaned CSV to the destination GCS bucket
+    gcs_hook.upload(destination_bucket, source_object, cleaned_file_path)
 
 with DAG(
-    "etl",
+    "etl_with_cleaning_transfer",
     start_date=timezone.datetime(2024, 5, 4),
-    schedule_interval="@daily",
+    schedule_interval=None,  # Set None for manual runs if appropriate
     tags=["madt_final_project_shamir"],
+    catchup=False  # To avoid backfilling DAG runs
 ) as dag:
 
-    # Dummy start task
     start = DummyOperator(
-        task_id='start',
-        dag=dag,
+        task_id='start'
     )
 
-    # Task to load data into Google Cloud Storage
-    upload_file_customer_to_warehouse = GCSToGCSOperator(
-        task_id="upload_file_customer_to_warehouse",
-        source_bucket="rawdata-madt-finalproject",
-        source_objects=["customer.csv"],
-        destination_bucket="storage-madt-finalproject",
-        destination_object="customer.csv",
-        gcp_conn_id='my_gcp_conn'
-    )
+    # List of files to process
+    files = [
+        'customer.csv', 'invoice.csv', 'invoice_trans.csv', 'invoice_type.csv',
+        'product.csv', 'reorder.csv', 'store.csv', 'zoning.csv', 'fact.csv'
+    ]
 
-    upload_file_invoice_to_warehouse = GCSToGCSOperator(
-        task_id="upload_file_invoice_to_warehouse",
-        source_bucket="rawdata-madt-finalproject",
-        source_objects=["invoice.csv"],
-        destination_bucket="storage-madt-finalproject",
-        destination_object="invoice.csv",
-        gcp_conn_id='my_gcp_conn'
-    )
+    # Task group to clean and transfer CSV files from raw bucket to storage bucket
+    with TaskGroup("clean_and_transfer_csv_files") as clean_and_transfer_group:
+        clean_and_transfer_tasks = [
+            PythonOperator(
+                task_id=f"clean_and_transfer_{file.split('.')[0]}",
+                python_callable=clean_and_transfer_csv,
+                op_args=[
+                    'rawdata-madt-finalproject',  # Source bucket
+                    file,                         # Source file
+                    'storage-madt-finalproject',  # Destination bucket
+                    'my-gcp-conn'                 # GCP connection ID
+                ],
+            
+            ) for file in files
+        ]
 
-    upload_file_invoice_trans_to_warehouse = GCSToGCSOperator(
-        task_id="upload_file_invoice_trans_to_warehouse",
-        source_bucket="rawdata-madt-finalproject",
-        source_objects=["invoice_trans.csv"],
-        destination_bucket="storage-madt-finalproject",
-        destination_object="invoice_trans.csv",
-        gcp_conn_id='my_gcp_conn'
-    )
-
-    upload_file_invoice_type_to_warehouse = GCSToGCSOperator(
-        task_id="upload_file_invoice_type_to_warehouse",
-        source_bucket="rawdata-madt-finalproject",
-        source_objects=["invoice_type.csv"],
-        destination_bucket="storage-madt-finalproject",
-        destination_object="invoice_type.csv",
-        gcp_conn_id='my_gcp_conn'
-    )
-
-    upload_file_product_to_warehouse = GCSToGCSOperator(
-        task_id="upload_file_product_to_warehouse",
-        source_bucket="rawdata-madt-finalproject",
-        source_objects=["product.csv"],
-        destination_bucket="storage-madt-finalproject",
-        destination_object="product.csv",
-        gcp_conn_id='my_gcp_conn'
-    )
-
-    upload_file_reorder_to_warehouse = GCSToGCSOperator(
-        task_id="upload_file_reorder_to_warehouse",
-        source_bucket="rawdata-madt-finalproject",
-        source_objects=["reorder.csv"],
-        destination_bucket="storage-madt-finalproject",
-        destination_object="reorder.csv",
-        gcp_conn_id='my_gcp_conn'
-    )
-
-    upload_file_store_to_warehouse = GCSToGCSOperator(
-        task_id="upload_file_store_to_warehouse",
-        source_bucket="rawdata-madt-finalproject",
-        source_objects=["store.csv"],
-        destination_bucket="storage-madt-finalproject",
-        destination_object="store.csv",
-        gcp_conn_id='my_gcp_conn'
-    )
-
-    upload_file_zoning_to_warehouse = GCSToGCSOperator(
-        task_id="upload_file_zoning_to_warehouse",
-        source_bucket="rawdata-madt-finalproject",
-        source_objects=["zoning.csv"],
-        destination_bucket="storage-madt-finalproject",
-        destination_object="zoning.csv",
-        gcp_conn_id='my_gcp_conn'
-    )
-
-    upload_file_fact_to_warehouse = GCSToGCSOperator(
-        task_id="upload_file_fact_to_warehouse",
-        source_bucket="rawdata-madt-finalproject",
-        source_objects=["fact.csv"],
-        destination_bucket="storage-madt-finalproject",
-        destination_object="fact.csv",
-        gcp_conn_id='my_gcp_conn'
-    )
-#------------------------------------------------------------------------------------------------------#
-
+    # Create dataset task
     create_dataset = BigQueryCreateEmptyDatasetOperator(
         task_id='create_dataset',
         dataset_id='finalproject_data',
-        gcp_conn_id='my_gcp_conn',
+        gcp_conn_id='my-gcp-conn',
     )
 
-#-----------------------------------------------------------------------------------------------------#
+    # Task group for GCS to BigQuery transfers (using cleaned files from storage bucket)
+    with TaskGroup("gcs_to_bigquery") as gcs_to_bq_group:
+        gcs_to_bq_tasks = [
+            GCSToBigQueryOperator(
+                task_id=f"gcs_to_bq_{file.split('.')[0]}",
+                bucket='storage-madt-finalproject',
+                source_objects=[file],
+                destination_project_dataset_table=f'madt-finalproject.finalproject_data.{file.split(".")[0]}',
+                write_disposition='WRITE_TRUNCATE',
+                create_disposition='CREATE_IF_NEEDED',
+                gcp_conn_id='my-gcp-conn',
+                skip_leading_rows =1, #skip the header row during import
+                retries=3,  # Add retries
+                retry_delay=datetime.timedelta(minutes=5)  # Corrected retry delay
+            ) for file in files
+        ]
 
-    gcs_to_bq_customer = GCSToBigQueryOperator(
-        task_id="gcs_to_bq_customer",
-        bucket='storage-madt-finalproject',
-        source_objects=['customer.csv'],
-        destination_project_dataset_table='finalproject_data.customer',
-        write_disposition='WRITE_TRUNCATE',
-        create_disposition='CREATE_IF_NEEDED',
-        gcp_conn_id='my_gcp_conn'
-    )
-
-    gcs_to_bq_invoice = GCSToBigQueryOperator(
-        task_id="gcs_to_bq_invoice",
-        bucket='storage-madt-finalproject',
-        source_objects=['invoice.csv'],
-        destination_project_dataset_table='finalproject_data.invoice',
-        write_disposition='WRITE_TRUNCATE',
-        create_disposition='CREATE_IF_NEEDED',
-        gcp_conn_id='my_gcp_conn'
-    )
-
-    gcs_to_bq_invoice_trans = GCSToBigQueryOperator(
-        task_id="gcs_to_bq_invoice_trans",
-        bucket='storage-madt-finalproject',
-        source_objects=['invoice_trans.csv'],
-        destination_project_dataset_table='finalproject_data.invoice_trans',
-        write_disposition='WRITE_TRUNCATE',
-        create_disposition='CREATE_IF_NEEDED',
-        gcp_conn_id='my_gcp_conn'
-    )
-
-    gcs_to_bq_invoice_type = GCSToBigQueryOperator(
-        task_id="gcs_to_bq_invoice_type",
-        bucket='storage-madt-finalproject',
-        source_objects=['invoice_type.csv'],
-        destination_project_dataset_table='finalproject_data.invoice_type',
-        write_disposition='WRITE_TRUNCATE',
-        create_disposition='CREATE_IF_NEEDED',
-        gcp_conn_id='my_gcp_conn'
-    )
-
-    gcs_to_bq_product = GCSToBigQueryOperator(
-        task_id="gcs_to_bq_product",
-        bucket='storage-madt-finalproject',
-        source_objects=['product.csv'],
-        destination_project_dataset_table='finalproject_data.product',
-        write_disposition='WRITE_TRUNCATE',
-        create_disposition='CREATE_IF_NEEDED',
-        gcp_conn_id='my_gcp_conn'
-    )
-
-    gcs_to_bq_reorder = GCSToBigQueryOperator(
-        task_id="gcs_to_bq_reorder",
-        bucket='storage-madt-finalproject',
-        source_objects=['reorder.csv'],
-        destination_project_dataset_table='finalproject_data.reorder',
-        write_disposition='WRITE_TRUNCATE',
-        create_disposition='CREATE_IF_NEEDED',
-        gcp_conn_id='my_gcp_conn'
-    )
-
-    gcs_to_bq_store = GCSToBigQueryOperator(
-        task_id="gcs_to_bq_store",
-        bucket='storage-madt-finalproject',
-        source_objects=['store.csv'],
-        destination_project_dataset_table='finalproject_data.store',
-        write_disposition='WRITE_TRUNCATE',
-        create_disposition='CREATE_IF_NEEDED',
-        gcp_conn_id='my_gcp_conn'
-    )
-
-    gcs_to_bq_zoning = GCSToBigQueryOperator(
-        task_id="gcs_to_bq_zoning",
-        bucket='storage-madt-finalproject',
-        source_objects=['zoning.csv'],
-        destination_project_dataset_table='finalproject_data.zoning',
-        write_disposition='WRITE_TRUNCATE',
-        create_disposition='CREATE_IF_NEEDED',
-        gcp_conn_id='my_gcp_conn'
-    )
-
-    gcs_to_bq_fact = GCSToBigQueryOperator(
-        task_id="gcs_to_bq_fact",
-        bucket='storage-madt-finalproject',
-        source_objects=['fact.csv'],
-        destination_project_dataset_table='finalproject_data.fact',
-        write_disposition='WRITE_TRUNCATE',
-        create_disposition='CREATE_IF_NEEDED',
-        gcp_conn_id='my_gcp_conn'
-    )
-
-    # Dummy end task
     end = DummyOperator(
-        task_id='end',
-        dag=dag,
+        task_id='end'
     )
 
-    # Defining the task dependencies
-    start >> [
-        upload_file_customer_to_warehouse,
-        upload_file_invoice_to_warehouse,
-        upload_file_invoice_trans_to_warehouse,
-        upload_file_invoice_type_to_warehouse,
-        upload_file_product_to_warehouse,
-        upload_file_reorder_to_warehouse,
-        upload_file_store_to_warehouse,
-        upload_file_zoning_to_warehouse,
-        upload_file_fact_to_warehouse
-    ] >> create_dataset >> [
-        gcs_to_bq_customer,
-        gcs_to_bq_invoice,
-        gcs_to_bq_invoice_trans,
-        gcs_to_bq_invoice_type,
-        gcs_to_bq_product,
-        gcs_to_bq_reorder,
-        gcs_to_bq_store,
-        gcs_to_bq_zoning,
-        gcs_to_bq_fact
-    ] >> end
+    # Defining task dependencies
+    start >> clean_and_transfer_group >> create_dataset >> gcs_to_bq_group >> end
